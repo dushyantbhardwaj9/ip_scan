d6f810ebdec002cc823b608a9617960cadca49c3
dushyantbhardwaj9 <dushyantbhardwaj9@gmail.com>
2020-08-11 23:09:05 +0530

    More Info

--- refactored_ip_scan.py
+++ refactored_ip_scan.py
@@ -9,24 +9,35 @@ import socket
 		Run ping For ALl possible ip address then
 		For all Active ip address run for 65535 ports
 """
+
 """ For IP
 	Ping to check if the host is active or not
 	IF active then check for port availability
 """
+
 """ Last option is to do it by hostname
 	do a hostbyname process to get IP addr and then do the thing
 """
 
+""" Build a Parser to parse the input through CLI
+	Also Build a Help module with a few examples on how to run
+"""
+
 """ The Thing To be Done
-	1. First get an IP addr or a list of same
-	2. Check if that host is alive
-	3. If alive then check for all open ports
-	4. If not return the message saying host is down
+	1. First get an IP addr or a list of same out of CIDR or by resolving the hostname
+ 	2. Check if that host is alive
+	3. If alive then check for all open ports or look for only specified ports
+	4. If not return the error message saying host is down
 	5. Make it robust by input sanitization and displaying error messages
 
 	(optional)
 	1. Create a json file storing all genral port number and services  or find a way to determine service running 
 	2. Differentiate between TCP or UDP ports and how to perform check on them.
+"""
+
+""" Make the output messages consize and informative 
+	Make it in points and seperate output between two hosts
+	Try Printing Ouput Along with scanning to see the results live
 """
 
 def check_ports(ip):
