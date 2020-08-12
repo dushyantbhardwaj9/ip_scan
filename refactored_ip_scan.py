b0934c1f3dad976bdf3be1e0c67278809ae8383e
dushyantbhardwaj9 <dushyantbhardwaj9@gmail.com>
2020-08-13 03:40:28 +0530

    Sanitising input with REGEX

--- refactored_ip_scan.py
+++ refactored_ip_scan.py
@@ -68,13 +68,45 @@ def parse_input():
 	# take input through parsing
 	# sanitize input and return
 	parser = argparse.ArgumentParser(description = "Network Scanner")
-	parser.add_argument("host", help = "Hostname/IP/CIDR to be searched for " )
+	parser.add_argument("host", help = "Hostname/IP/CIDR " )
 	parser.add_argument("-p","--port", nargs = "*", help = "Specific ports to enumerate for" )
 
 	inputs = parser.parse_args()
-
+	if inputs.port:
+		return (inputs.host, inputs.port)
+	else:
+		return (inputs.host,None)
 
 if "__name__" == "__main__":
 	# take input 
+	hosts_input,ports_input = parse_input()
+
+	# Regex's to validate input
+	comma_sep_value_regex = "^[0-9]+(,[0-9]+)+$"
+	range_ports_regex = "^(\d+)(?:-(\d+))?$"
+	individual_port_regex = "^(0|[1-9][0-9]{0,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$"
+
+	if re.fullmatch(comma_sep_value_regex, ports_input):
+		value = re.fullmatch(comma_sep_value_regex, ports_input)
+		ports = value.group().split(',')
+		if max(ports) > 65535 or min(ports):
+			# Show Error
+			return
+
+	elif re.fullmatch(range_ports_regex, ports_input):
+		value = re.fullmatch(range_ports_regex, ports_input)
+		start,end = value.group().split('-')
+		start,end = int(start),int(end)
+		if start < 0 or start > 65535 or end < 0 or end > 65535:
+			# Show error
+			return
+		ports = [port for port in range(start, end+1)]
+
+	elif re.fullmatch(individual_port_regex, ports_input):
+		value = re.fullmatch(individual_port_regex, ports_input)
+		ports = [int(value.group())]
+
+	if ports == None:
+		ports = [x for x in range(1,65536)]
 	# check input with regex
 	# accordingly run the program 
