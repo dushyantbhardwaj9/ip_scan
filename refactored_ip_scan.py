50ab49b982acfbe6c7b5b4d315c809f743c3f44d
dushyantbhardwaj9 <dushyantbhardwaj9@gmail.com>
2020-08-23 14:32:26 +0530

    Added netmask validation in CIDR validation

--- refactored_ip_scan.py
+++ refactored_ip_scan.py
@@ -156,6 +156,27 @@ def validate_host(hosts_input):
 		value = re.fullmatch(pattern = cidr_block_regex, string =hosts_input)
 		host = value.group()
 
+		net_mask = host.split("/")[1]
+		if net_mask <= 32 and net_mask => 24:
+			network_ip = host.split(".")
+			network_ip[3] = "".join(["0/",net_mask])
+			host = ".".join(network_ip)
+		
+		elif net_mask <= 23 and net_mask => 12:
+			network_ip = host.split(".")
+			network_ip[2] = "0"
+			network_ip[3] = "".join(["0/",net_mask])
+			host = ".".join(network_ip)
+
+		elif net_mask <= 11 and net_mask => 2:
+			network_ip = host.split(".")
+			network_ip[1] = "0"
+			network_ip[2] = "0"
+			network_ip[3] = "".join(["0/",net_mask])
+			host = ".".join(network_ip)
+		else:
+			print("Invalid Netmask")
+			
 		net4 = ipaddress.ip_network(host)
 		for x in net4.hosts():
 			hosts.append(str(x))
